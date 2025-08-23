1685da46a970f94903384d5793fdaf84923c4d42
Enzo Dornelles Italiano <enzo.ditaliano@gmail.com>
2025-08-24 00:28:55 +0100

    start project primary routes

---
+++ server.ts
@@ -0,0 +1,63 @@
+import fastify from 'fastify'
+import { randomUUID } from 'node:crypto'
+
+const server = fastify({
+    logger: {
+        transport: {
+            target: 'pino-pretty',
+            options: {
+                translateTime: 'HH:MM:ss Z',
+                ignore: 'pid,hostname',
+            },
+        },
+    }
+})
+
+const courses = [
+    { id: "1", title: "Curso de Node" },
+    { id: "2", title: "Curso de React" },
+    { id: "3", title: "Curso de Vue" }
+]
+
+server.get('/courses', () => {
+    return { courses }
+})
+
+server.get('/courses/:id', (request, reply) => {
+    type Params = {
+        id: string
+    }
+
+    const params = request.params as Params
+    const courseId = params.id
+
+
+    const course = courses.find(course => course.id === courseId)
+
+    if(course){
+        return {course}
+    }
+
+    return reply.status(404)
+})
+
+server.post('/courses', (request, reply) => {
+    type Body = {
+        title: string
+    }
+    
+    const courseId = randomUUID()
+    const body = request.body as Body
+    const courseTitle = body.title
+    if (!courseTitle) {
+        return reply.status(400).send({message: 'Titulo obrigatório'})
+    }
+
+    courses.push({ id: courseId, title: courseTitle })
+    
+    return reply.status(201).send({ courseId })
+})
+
+server.listen({ port: 3333 }).then(()=>{
+    console.log('HTTP server running!')
+})
